828e399ccd35473d6ace9c9b7b685300b25ff07b
Kyle Barton <Kyle.Barton@mscience.com>
2017-04-21 16:48:39 -0400

    apparantly I have to use double quotes in .cmd files

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -1,4 +1,4 @@
 ECHO OFF
-& 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe' ..\HelloFriend\HelloFriend\HelloFriend.csproj /t:package /p:DeployIisAppPath="Default Web Site"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ..\HelloFriend\HelloFriend\HelloFriend.csproj /t:package /p:DeployIisAppPath="Default Web Site"
 eb deploy
 ECHO ON
