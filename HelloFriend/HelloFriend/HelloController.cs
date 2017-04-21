9fc82ecc08d65fe676f27b4c85984f4cb3c523ab
Kyle Barton <Kyle.Barton@mscience.com>
2017-04-21 16:37:10 -0400

    adding another endpoint

--- HelloFriend/HelloFriend/HelloController.cs
+++ HelloFriend/HelloFriend/HelloController.cs
@@ -4,16 +4,25 @@ namespace HelloFriend
 {
     public class HelloController : ApiController
     {
+        [HttpGet]
         [Route("hello/world")]
         public IHttpActionResult Get()
         {
             return Ok("Hello World!");
         }
 
+        [HttpGet]
         [Route("hello/v2/{name}")]
         public IHttpActionResult Get(string name)
         {
             return Ok($"Hello, {name}");
+        }
+
+        [HttpGet]
+        [Route("insult/{name}")]
+        public IHttpActionResult Insult(string name)
+        {
+            return Ok($"You're ugly, {name}!");
         }
     }
 }
