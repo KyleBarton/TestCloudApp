3711e546172c1b1e917462ad7a53c2a9f2fb02c7
Kyle Barton <Kyle.Barton@mscience.com>
2017-04-21 11:02:05 -0400

    hello world

---
+++ HelloFriend/HelloFriend/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace HelloFriend
+{
+    public class HelloController : ApiController
+    {
+        [Route("hello/world")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
---
+++ HelloFriend/HelloFriend/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HelloFriend.Startup))]
+
+namespace HelloFriend
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}
